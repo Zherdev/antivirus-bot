257e5cc8d8094e373ad20e3f83ebd2e68c680c52
Ivan Zherdev <zherdevivanp@gmail.com>
2020-05-13 18:49:53 +0000

    admin get logs

--- pkg/admin/admin.go
+++ pkg/admin/admin.go
@@ -7,8 +7,10 @@ package admin
 import (
 	"antivirus-bot/pkg/configuration"
 	"context"
-	"encoding/json"
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/neko-neko/echo-logrus/v2"
+	"github.com/neko-neko/echo-logrus/v2/log"
 	"github.com/sirupsen/logrus"
 	"io"
 	"net/http"
@@ -16,9 +18,16 @@ import (
 	"time"
 )
 
+// Так приходят запросы в json'е
+type requestBody struct {
+	AdminToken string
+}
+
 type Admin struct {
+	e      *echo.Echo
 	config *configuration.Configuration
 	logger *logrus.Logger
+	stop   context.CancelFunc
 }
 
 func NewAdmin(logOut io.Writer, config *configuration.Configuration) (*Admin, error) {
@@ -26,97 +35,88 @@ func NewAdmin(logOut io.Writer, config *configuration.Configuration) (*Admin, er
 		return nil, fmt.Errorf("nil params in NewAdmin")
 	}
 
-	logger := logrus.New()
-	logger.SetLevel(logrus.TraceLevel)
+	logger := log.Logger()
+	logger.Logger.SetLevel(logrus.TraceLevel)
 	logger.SetFormatter(&logrus.JSONFormatter{
-		TimestampFormat: "30-12-2006 15:04:05",
+		TimestampFormat: "2006-01-02 15:04:05",
 	})
 	logger.SetOutput(logOut)
 
+	e := echo.New()
+	e.Server.ReadTimeout = time.Duration(config.AdminTimeout) * time.Second
+	e.Server.WriteTimeout = time.Duration(config.AdminTimeout) * time.Second
+	e.Logger = logger
+	e.Use(middleware.Logger())
+
 	return &Admin{
+		e:      e,
 		config: config,
-		logger: logger,
+		logger: logger.Logger,
 	}, nil
 }
 
+// Авторизация. Проверяем админский токен
+func (a *Admin) authMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(ctx echo.Context) error {
+		method := "authMiddleware"
+
+		data := &requestBody{}
+		err := ctx.Bind(data)
+		if err != nil {
+			a.logger.
+				WithField("method", method).
+				Warn("can't unmarshal json")
+			return ctx.JSON(http.StatusBadRequest, "bad request body")
+		}
+
+		if data.AdminToken != a.config.AdminToken {
+			a.logger.
+				WithField("method", method).
+				Warn("bad AdminToken")
+			return ctx.JSON(http.StatusUnauthorized, "bad auth data")
+		}
+
+		ctx.Set("requestBody", data)
+		return next(ctx)
+	}
+}
+
 // Ожидает команды остановки приложения через http POST
-func (a *Admin) shutdownHandler(stop context.CancelFunc, w http.ResponseWriter, r *http.Request) {
-	method := "Admin.ShutdownHandler"
+func (a *Admin) shutdownHandler(ctx echo.Context) error {
+	method := "shutdownHandler"
+	a.logger.
+		WithField("method", method).
+		Info("shut down")
 
-	if r.Method != http.MethodPost {
-		a.logger.
-			WithField("method", method).
-			Warn("GET method used")
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
+	a.stop()
 
-	reqMap := map[string]string{}
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&reqMap)
-	if err != nil {
-		a.logger.
-			WithField("method", method).
-			Error(err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	token, ok := reqMap["AdminToken"]
-	if !ok {
-		a.logger.
-			WithField("method", method).
-			Warn("no AdminToken")
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	if token != a.config.AdminToken {
-		a.logger.
-			WithField("method", method).
-			Warn("bad AdminToken")
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	stop()
+	return ctx.JSON(http.StatusOK, "shut down")
 }
 
 // Старт админки
 func (a *Admin) Run(stop context.CancelFunc, ctx context.Context, papaWg *sync.WaitGroup) {
-	srvMux := http.NewServeMux()
-	srvWg := &sync.WaitGroup{}
+	a.stop = stop
 
-	srvMux.HandleFunc(a.config.AdminShutdownPath, func(w http.ResponseWriter, r *http.Request) {
-		a.shutdownHandler(stop, w, r)
-	})
+	fileServer := http.FileServer(http.Dir(a.config.AdminLogsPath)) // для получения логов
+	echoFileHandler := echo.WrapHandler(http.StripPrefix(a.config.AdminGetLogsPath, fileServer))
 
-	srv := &http.Server{
-		Addr:         a.config.AdminHost + ":" + a.config.AdminPort,
-		Handler:      srvMux,
-		ReadTimeout:  time.Duration(a.config.AdminTimeout) * time.Second,
-		WriteTimeout: time.Duration(a.config.AdminTimeout) * time.Second,
-	}
+	a.e.POST(a.config.AdminShutdownPath, a.shutdownHandler, a.authMiddleware)
+	a.e.POST(a.config.AdminGetLogsPath + "*", echoFileHandler, a.authMiddleware)
 
-	srvWg.Add(1)
 	go func() {
-		defer srvWg.Done()
-		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-			a.logger.
-				WithField("method", "Admin.Run.ListenAndServe").
-				Error(err)
-			stop()
+		if err := a.e.Start(a.config.AdminHost + ":" + a.config.AdminPort); err != nil {
+			a.e.Logger.Info("shutting down the server")
 		}
 	}()
 
 	<-ctx.Done()
-	if err := srv.Shutdown(ctx); err != nil {
+	ctxForShutDown, shutDownEcho := context.WithCancel(context.Background())
+	defer shutDownEcho()
+	if err := a.e.Shutdown(ctxForShutDown); err != nil {
 		a.logger.
-			WithField("method", "Admin.Run.Shutdown").
+			WithField("method", "Run").
 			Error(err)
 	}
-	srvWg.Wait()
 
 	a.logger.Info("admin is done")
 	papaWg.Done()
